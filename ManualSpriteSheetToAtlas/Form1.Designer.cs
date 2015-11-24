4a597d61ef6d63bc9a2cb1c480aad08cd8da881d
James Skemp <strivinglife@gmail.com>
2015-11-24 08:16:23 -0600

    Can now select an image to load.

--- ManualSpriteSheetToAtlas/Form1.Designer.cs
+++ ManualSpriteSheetToAtlas/Form1.Designer.cs
@@ -28,12 +28,80 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.panelOriginalImage = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.Filter = "Image Files(*.jpg; *.png; *.gif; *.bmp)| *.jpg; *.png; *.gif; *.bmp";
+			this.openFileDialog1.Title = "Select a sprite sheet to load";
+			// 
+			// panelOriginalImage
+			// 
+			this.panelOriginalImage.BackColor = System.Drawing.Color.White;
+			this.panelOriginalImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.panelOriginalImage.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panelOriginalImage.Location = new System.Drawing.Point(0, 40);
+			this.panelOriginalImage.Name = "panelOriginalImage";
+			this.panelOriginalImage.Size = new System.Drawing.Size(921, 725);
+			this.panelOriginalImage.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.White;
+			this.panel2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+			this.panel2.Location = new System.Drawing.Point(948, 40);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(262, 725);
+			this.panel2.TabIndex = 1;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(1210, 40);
+			this.menuStrip1.TabIndex = 2;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 36);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1210, 765);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panelOriginalImage);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
 			this.Text = "Form1";
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Panel panelOriginalImage;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
 	}
 }
 
--- ManualSpriteSheetToAtlas/Form1.cs
+++ ManualSpriteSheetToAtlas/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,24 @@ namespace ManualSpriteSheetToAtlas
 		public Form1()
 		{
 			InitializeComponent();
+
+			if (openFileDialog1.ShowDialog() != DialogResult.OK)
+			{
+				MessageBox.Show("You must select an image to continue.");
+
+				if (openFileDialog1.ShowDialog() != DialogResult.OK)
+				{
+					// The user has failed twice; quit.
+					Application.Exit();
+				}
+			}
+
+			var imageFile = Image.FromFile(openFileDialog1.FileName);
+
+			if (imageFile != null)
+			{
+				panelOriginalImage.BackgroundImage = imageFile;
+			}
 		}
 	}
 }
