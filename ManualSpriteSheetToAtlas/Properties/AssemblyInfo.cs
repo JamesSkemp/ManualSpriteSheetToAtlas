4833e97b084f211df2de4806e6d1fa70cf03e177
James Skemp <strivinglife@gmail.com>
2015-11-24 07:29:25 -0600

    Updated application meta data.

--- ManualSpriteSheetToAtlas/Properties/AssemblyInfo.cs
+++ ManualSpriteSheetToAtlas/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ManualSpriteSheetToAtlas")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Manual Sprite Sheet to Atlas")]
+[assembly: AssemblyDescription("Allows creation of an atlas file for a sprite sheet.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("James Skemp - jamesrskemp.com")]
 [assembly: AssemblyProduct("ManualSpriteSheetToAtlas")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © James Skemp 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
