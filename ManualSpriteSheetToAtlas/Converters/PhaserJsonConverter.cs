0571e4a22b06fcb076efea65f2dfec22c24a48e8
James Skemp <strivinglife@gmail.com>
2015-12-01 11:31:52 -0600

    Started working on custom JSON.net converter. Documentation regarding this sucks.

---
+++ ManualSpriteSheetToAtlas/Converters/PhaserJsonConverter.cs
@@ -0,0 +1,43 @@
+﻿using ManualSpriteSheetToAtlas.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManualSpriteSheetToAtlas.Converters
+{
+	public class PhaserJsonConverter : JsonConverter
+	{
+		public override bool CanConvert(Type objectType)
+		{
+			return typeof(AtlasDefinition).IsAssignableFrom(objectType);
+		}
+
+		public override bool CanRead
+		{
+			get
+			{
+				// TODO allow reading at some point.
+				return false;
+			}
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+		{
+			// Until CanRead is true this isn't needed.
+			throw new NotImplementedException();
+		}
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			JToken t = JToken.FromObject(value);
+			Console.WriteLine(value);
+			Console.WriteLine(t);
+			Console.WriteLine(t.Type);
+			t.WriteTo(writer);
+		}
+	}
+}
