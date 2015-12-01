1af69684f5bc6836f003f9a693c91a6f08ea399c
James Skemp <strivinglife@gmail.com>
2015-12-01 08:27:44 -0600

    Created new class to store information about sprites drawn.

---
+++ ManualSpriteSheetToAtlas/SpriteDefinition.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManualSpriteSheetToAtlas
+{
+	/// <summary>
+	/// Definition of an individual sprite.
+	/// </summary>
+	public class SpriteDefinition
+	{
+		public string Name { get; set; }
+		public int X { get; set; }
+		public int Y { get; set; }
+		public int Width { get; set; }
+		public int Height { get; set; }
+
+		public SpriteDefinition()
+		{
+		}
+
+		public SpriteDefinition(string name, int x, int y, int width, int height) : base()
+		{
+			this.Name = name;
+			this.X = x;
+			this.Y = y;
+			this.Width = width;
+			this.Height = height;
+		}
+	}
+}
